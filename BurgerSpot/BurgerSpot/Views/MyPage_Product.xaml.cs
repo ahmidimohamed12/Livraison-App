55e3368ec6cb925774d62bcb5f8053ce911f6d40
ahmidimohamed12 <ahmidinador123@gmail.com>
2020-12-18 10:42:23 +0100

    add

---
+++ BurgerSpot/BurgerSpot/Views/MyPage_Product.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace BurgerSpot.Views
+{
+    public partial class MyPage_Product : ContentPage
+    {
+        public MyPage_Product()
+        {
+            InitializeComponent();
+        }
+    }
+}
